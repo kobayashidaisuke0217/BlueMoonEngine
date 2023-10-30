7b79f9478f5929739d0b585b0ef81334da1851e4
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-10-30 14:27:00 +0900

    60FPS固定

--- Engine/base/DirectXCommon.cpp
+++ Engine/base/DirectXCommon.cpp
@@ -19,6 +19,7 @@ DirectXCommon* DirectXCommon::GetInstance()
 
 void DirectXCommon::Initialize(WinApp* win, int32_t backBufferWidth, int32_t backBufferHeight)
 {
+	InitializeFixFPS();
 	//resourceLeak = new LeakCheck();
 	winApp_ = win;
 	backBufferWidth_ = backBufferWidth;
@@ -232,6 +233,7 @@ void DirectXCommon::PostDraw() {
 		//イベント待つ
 		WaitForSingleObject(fenceEvent_, INFINITE);
 	}
+	updateFixFPS();
 	//次のフレーム用のコマンドリストを準備
 	hr_ = commandAllocator_->Reset();
 	assert(SUCCEEDED(hr_));
@@ -376,6 +378,27 @@ void DirectXCommon::CreateDebug()
 		//infoQueue->Release();
 	}
 #endif
+}
+
+void DirectXCommon::InitializeFixFPS()
+{
+	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
+	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+
+	std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+	if (elapsed < kMinTime) {
+		while (std::chrono::steady_clock::now()-reference_<kMinTime)
+		{
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}reference_= std::chrono::steady_clock::now();
+}
+
+void DirectXCommon::updateFixFPS()
+{
+	reference_ = std::chrono::steady_clock::now();
 }
 
 
